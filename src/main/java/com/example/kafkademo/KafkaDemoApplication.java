81f96262deda37b91ee025bcea57c3ec954d76db
Anupama <anupama80286@gmail.com>
2024-02-18 21:50:32 +0530

    initial change

---
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.kafkademo;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+@SpringBootApplication
+public class KafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoApplication.class);
+	}
+
+
+}
